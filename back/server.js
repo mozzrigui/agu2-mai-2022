e6a9f1885c5e81413e86a87bff582818b8ddb43c
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-05-16 12:11:25 +0200

    dev: express middleware example

--- back/server.js
+++ back/server.js
@@ -3,6 +3,12 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.use((req, res, next) => {
+  console.log("req: ", req.url);
+  res.status(258);
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
