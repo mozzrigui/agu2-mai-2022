d333e0a8aec6393962cd24abdf196ec5f6c6f3ea
contact.trigui.moez@gmail.com <410414417>
2022-05-24 14:38:03 +0100

    Observable added

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,26 +1,25 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
 import { HomeComponent } from './routes/home/home.component';
 import { LegalComponent } from './routes/legal/legal.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ArticleService } from './services/article.service';
+import { HttpArticleService } from './services/http-article.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    LegalComponent
-  ],
+  declarations: [AppComponent, HomeComponent, LegalComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     LayoutModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [{ provide: ArticleService, useClass: HttpArticleService }],
   bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ front/src/app/services/http-article.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { ArticleService } from './article.service';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HttpArticleService extends ArticleService {
+  constructor(private http: HttpClient) {
+    super();
+    console.log('http article instantiated');
+  }
+
+  override async refresh() {
+    this.http.get('http://localhost:3000/api/articles').subscribe();
+  }
+}
