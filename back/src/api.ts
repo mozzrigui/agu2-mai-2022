0a433620291c2fc1577e5a653622b9c7d4300a7d
contact.trigui.moez@gmail.com <410414417>
2022-05-25 08:48:52 +0100

    added remove on select

--- back/src/api.ts
+++ back/src/api.ts
@@ -1,18 +1,22 @@
-import { Router, json } from "express";
+import { json, Router } from "express";
+import { serialize } from "v8";
 import { Article } from "./interfaces/article";
+import { v4 as uuidv4 } from "uuid";
 
-const articles: Article[] = [
+let articles: Article[] = [
   {
+    id: "1",
     name: "Pelle",
     price: 40,
     qty: 5,
   },
   {
+    id: "2",
     name: "Pelle3",
     price: 40,
     qty: 5,
   },
-  { name: "Pelleeeee2", price: 70, qty: 5 },
+  { id: "3", name: "Pelleeeee2", price: 70, qty: 5 },
 ];
 
 const app = Router();
@@ -28,9 +32,29 @@ app.get("/articles", (req, res) => {
 app.use(json());
 
 app.post("/articles", (req, res) => {
-  const article = req.body;
-  articles.push(article);
-  res.status(201).end();
+  try {
+    const article = req.body as Article;
+    article.id = uuidv4();
+    articles.push(article);
+    res.status(201).end();
+  } catch (err) {
+    console.log("err:", err);
+  } finally {
+    res.status(500).end();
+  }
+});
+
+app.delete("/articles", (req, res) => {
+  try {
+    const ids = req.body as string[];
+    console.log("ids:", ids);
+    articles = articles.filter((a) => !ids.includes(a.id));
+    res.status(204).end();
+  } catch (err) {
+    console.log("err:", err);
+  } finally {
+    res.status(500).end();
+  }
 });
 
 export const api = app;
--- front/src/app/services/http-article.service.ts
+++ front/src/app/services/http-article.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@angular/core';
-import { ArticleService } from './article.service';
 import { HttpClient } from '@angular/common/http';
-import { Article } from '../interfaces/article';
+import { Injectable } from '@angular/core';
 import { delay, lastValueFrom } from 'rxjs';
+import { Article } from '../interfaces/article';
+import { ArticleService } from './article.service';
 const ARTICLES_URL = 'http://localhost:3000/api/articles';
 
 @Injectable({
@@ -16,7 +16,7 @@ export class HttpArticleService extends ArticleService {
 
   override async refresh() {
     const articles = await lastValueFrom(
-      this.http.get<Article[]>(ARTICLES_URL).pipe(delay(2000))
+      this.http.get<Article[]>(ARTICLES_URL).pipe(delay(10))
     );
     console.log('articles: ', articles);
     this.articles = articles;
@@ -24,10 +24,22 @@ export class HttpArticleService extends ArticleService {
   }
 
   override async add(article: Article): Promise<void> {
-    super.add(article);
-
+    await super.add(article);
     await lastValueFrom(
-      this.http.post<Article[]>(ARTICLES_URL, article).pipe(delay(1000))
+      this.http.post<Article[]>(ARTICLES_URL, article).pipe(delay(10))
+    );
+  }
+
+  override async remove(selectedArticles: Set<Article>): Promise<void> {
+    await super.remove(selectedArticles);
+    const ids = [...selectedArticles].map((a) => a.id);
+    await lastValueFrom(
+      this.http.delete<void>(ARTICLES_URL, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(ids),
+      })
     );
   }
 }
