8dc08e08f95a55c62e9296248d58bb341ba6cd00
contact.trigui.moez@gmail.com <410414417>
2022-05-25 10:28:50 +0100

    autofocus

--- front/src/app/widgets/autofocus.directive.ts
+++ front/src/app/widgets/autofocus.directive.ts
@@ -1,15 +1,25 @@
-import { Directive, ElementRef, OnInit } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[appAutofocus]',
 })
 export class AutofocusDirective implements OnInit {
+  @Input()
+  appAutofocus = 'focus';
+
   constructor(private elt: ElementRef<HTMLElement>) {
     console.log('instantiate autofocus');
     console.log(this.elt.nativeElement);
   }
 
   ngOnInit(): void {
+    if (this.appAutofocus === 'select') {
+      if (!(this.elt.nativeElement instanceof HTMLInputElement)) {
+        throw new Error('Directive must be on an input elt');
+      }
+      this.elt.nativeElement.select();
+      return;
+    }
     this.elt.nativeElement.focus();
   }
 }
